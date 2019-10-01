07dee7fda9c97953decc5650cb3a9b909733abf0
Kurganova Alexandra <kurganova06.1998@gmail.com>
2019-10-01 22:52:44 +0300

    проработана mind-map 2, сделан макет и описание

---
+++ IdealVisual/model/UserModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserModel.swift
+//  IdealVisual
+//
+//  Created by a.kurganova on 30/09/2019.
+//  Copyright © 2019 a.kurganova. All rights reserved.
+//
+
+import Foundation
---
+++ IdealVisual/view/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  IdealVisual
+//
+//  Created by a.kurganova on 01/10/2019.
+//  Copyright © 2019 a.kurganova. All rights reserved.
+//
+
+import Foundation
