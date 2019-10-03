bd8c1550d3b70ed9d50a3081e13d734304ffec72
Kurganova Alexandra <kurganova06.1998@gmail.com>
2019-10-03 23:11:35 +0300

    редизайн макета, фикс ридми

---
+++ IdealVisual/view/Buttons.swift
@@ -0,0 +1,9 @@
+//
+//  Buttons.swift
+//  IdealVisual
+//
+//  Created by a.kurganova on 03/10/2019.
+//  Copyright © 2019 a.kurganova. All rights reserved.
+//
+
+import Foundation
---
+++ IdealVisual/view/TabBarItem.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarItem.swift
+//  IdealVisual
+//
+//  Created by a.kurganova on 03/10/2019.
+//  Copyright © 2019 a.kurganova. All rights reserved.
+//
+
+import UIKit
+
+class TabBarItem: UITabBarItem {
+
+}
